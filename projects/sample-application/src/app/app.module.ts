f58ffa815ff5c45e278b467eefb7694b121160af
SENBAGARAMAN MANOHARAN <senbagaraman.manoharan@siemens.com>
2023-02-09 17:44:29 +0530

    fix: consumed lib in app1

--- projects/sample-application/src/app/app.module.ts
+++ projects/sample-application/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SamplesharedlibraryModule } from 'projects/samplesharedlibrary/src/public-api';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SamplesharedlibraryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
