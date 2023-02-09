103d136dae7c8b55e623bb17ee20c1cc45251eef
SENBAGARAMAN MANOHARAN <senbagaraman.manoharan@siemens.com>
2023-02-09 17:34:46 +0530

    fix: added data in sharedlib

--- projects/samplesharedlibrary/src/lib/hello-component/hello-component.component.ts
+++ projects/samplesharedlibrary/src/lib/hello-component/hello-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-hello-component',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hello-component.component.css']
 })
 export class HelloComponentComponent implements OnInit {
+
+  @Input()  consumedApplicationName = '';
 
   constructor() { }
 
