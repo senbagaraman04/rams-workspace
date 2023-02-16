cfb3c8510d9f61a088c1e3288b739baee55e7031
Senbagaraman04 <senbagaraman04@gmail.com>
2023-02-16 20:35:51 +0530

    chore: package update

--- projects/samplesharedlibrary/src/public-api.ts
+++ projects/samplesharedlibrary/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of samplesharedlibrary
  */
 
-export * from './lib/samplesharedlibrary.service';
-export * from './lib/samplesharedlibrary.component';
 export * from './lib/samplesharedlibrary.module';
