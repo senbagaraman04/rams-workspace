e8bcb5376da75d62e8ed6df4d7629be490c86ec7
Senbagaraman04 <senbagaraman04@gmail.com>
2023-03-01 19:44:43 +0530

    fix: inject eror in sample 2

--- projects/sample-application2/src/app/app.module.ts
+++ projects/sample-application2/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { SamplesharedlibraryModule } from 'projects/samplesharedlibrary/src/public-api';
+import { SamplesharedlibraryModule } from '../../../samplesharedlibrary/src/public-api';
 
 import { AppComponent } from './app.component';
 
